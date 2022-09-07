70cef2904df18f2431a6031020fe31de2a861921
enzobonf <enzobonfx@gmail.com>
2022-09-06 21:56:30 -0300

    feat: refactor e update json server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
+const isDev = process.env.NODE_ENV === "development";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: "http://localhost:3333",
 });
